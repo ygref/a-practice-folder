20d8478a87a0e5c7005c194dde89c3598c2c3e6c
ygref <jason.wesley.hc@gmail.com>
2023-10-19 15:10:19 -0400

    Kenobi in JS

--- practice.js
+++ practice.js
@@ -4,9 +4,13 @@
 //   number = number + 5;
 // }
 
-var count = 0;
-while (count <= 144) {
-  console.log("Around the world");
-  console.log(count);
-  count += 1;
-}
+// var count = 0;
+// while (count <= 144) {
+//   console.log("Around the world");
+//   console.log(count);
+//   count += 1;
+// }
+
+var array = ["Hello", "there"];
+array.push("General", "Kenobi");
+console.log(array);
