3680d596fd82e25bfecbe29b576d097ea0653dfc
ygref <jason.wesley.hc@gmail.com>
2023-10-19 14:31:27 -0400

    around the world js

--- practice.js
+++ practice.js
@@ -1,5 +1,12 @@
-var number = 0;
-while (number <= 100) {
-  console.log(number);
-  number = number + 5;
+// var number = 0;
+// while (number <= 100) {
+//   console.log(number);
+//   number = number + 5;
+// }
+
+var count = 0;
+while (count <= 144) {
+  console.log("Around the world");
+  console.log(count);
+  count += 1;
 }
