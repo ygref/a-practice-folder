4234fe348f3cc12af4ad69d45cf280c3b2eaaa2c
ygref <jason.wesley.hc@gmail.com>
2023-10-19 14:27:33 -0400

    around the world

--- practice.rb
+++ practice.rb
@@ -6,9 +6,16 @@
 # end
 # end
 
-number = 0
+# number = 0
 
-while number <= 100
-  puts number
-number += 5
+# while number <= 100
+#   puts number
+# number += 5
+# end
+
+phrase = "Around the world"
+index = 0
+while index <= 144
+  puts index.to_s + " " + phrase
+index += 1
 end
