8bed7a37c6320285ee174179e9bec42f8d88eec8
ygref <jason.wesley.hc@gmail.com>
2023-12-06 15:53:38 -0500

    nested loops conditionals JS

--- practice.js
+++ practice.js
@@ -26,3 +26,26 @@ const { endpoint } = require("mtgsdk/src/config");
 // const numbers = [2, 4, 5, 1, 8, 9, 7];
 // var evenNumbers = [];
 // index = 0;
+
+// Write a program that stores a customer's age and a movie's time in two separate variables. Then calculate the price of a movie ticket based on the following conditions:
+
+// If the age is 12 years old or younger, the ticket price is $5.
+// If the age is between 13 and 59 years old and the movie is before 6 PM, the ticket price is $7. After 6 PM, the ticket price is $10.
+// If the customer is 60 years old or older, the ticket price is $7.
+
+var customerAge = 60;
+var movieTime = 8;
+var ticketPrice;
+
+if (customerAge <= 12) {
+  ticketPrice = 5;
+} else if (customerAge >= 13 && customerAge <= 59) {
+  if (movieTime < 18) {
+    ticketPrice = 7;
+  } else {
+    ticketPrice = 10;
+  }
+} else if (customerAge >= 60) {
+  ticketPrice = 7;
+}
+console.log(ticketPrice);
