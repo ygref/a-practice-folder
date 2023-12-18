5d491e30c27240c4cda201b6f06a2f8f5368f30f
ygref <jason.wesley.hc@gmail.com>
2023-12-18 18:59:56 -0500

    method practice JS subtraction

--- practice.js
+++ practice.js
@@ -59,7 +59,14 @@ const { endpoint } = require("mtgsdk/src/config");
 
 // Write a method that takes in a string and returns the string with all capital letters. Then run the method and print the result.
 
-function makeCaps(string) {
-  return string.toUpperCase();
+// function makeCaps(string) {
+//   return string.toUpperCase();
+// }
+// console.log(makeCaps("lowercase"));
+
+// Write a method that takes in two numbers and returns the first number subtracted by the second. Then run the method and print the result.
+
+function subtract(number1, number2) {
+  return number1 - number2;
 }
-console.log(makeCaps("lowercase"));
+console.log(subtract(10, 5));
