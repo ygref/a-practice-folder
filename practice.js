b42bbce236cd77072eb0e0e155dd9d8e50b60b7b
ygref <jason.wesley.hc@gmail.com>
2024-07-30 21:08:59 -0400

    Practice

--- practice.js
+++ practice.js
@@ -11,12 +11,26 @@
 //   count += 1;
 // }
 
-var array = ["Hello", "there"];
-array.push("General", "Kenobi");
-console.log(array);
+// var array = ["Hello", "there"];
+// array.push("General", "Kenobi");
+// console.log(array);
 
-var sentence = "";
-for (let index = 0; index < array.length; index++) {
-  sentence += array[index] + " ";
+// var sentence = "";
+// for (let index = 0; index < array.length; index++) {
+//   sentence += array[index] + " ";
+// }
+// console.log(sentence + "!");
+
+var numbers = [1, 2, 3];
+var newNumbers = [];
+index = 0
+
+while (index < numbers.length) {
+  number = numbers[index];
+  newNumbers.push(number * 3);
+  index = index + 1
 }
-console.log(sentence + "!");
+console.log(newNumbers)
+
+
+
